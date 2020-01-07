f1f9cec66ca0d1dd556b08cd15c1b8352ed2ec32
Asua Hostin <a01054394111@gmail.com>
2020-01-07 21:59:29 +0900

    feat: 현재 내가 로그인 상태인지 확인하는 함수 구현
    인증 서비스에 isLoined라는 함수를 추가함
    나의 상태를 알고 페이지 렌더링을 하기 위해

---
+++ client/src/@service/auth-service.js
@@ -0,0 +1,27 @@
+import { auth } from "data/http-request/auth-api";
+import { BrowserStorage } from "data/browser-storage";
+import { tokenMapper } from "data/browser-storage/type-mapper/token-mapper";
+import { StorageType } from "enum/storage";
+
+const AuthService = function() {
+  this.authApi = auth;
+  this.storage = new BrowserStorage("token", StorageType.LOCAL, tokenMapper);
+};
+
+AuthService.prototype.login = async function(id, password) {
+  try {
+    const result = await this.authApi.login(id, password);
+    this.storage.set(result.data.token);
+    return "성공";
+  } catch (error) {
+    throw error;
+  }
+};
+
+AuthService.prototype.isLogined = function() {
+  const token = this.storage.get();
+  if (token && token["token"]) return true;
+  return false;
+};
+
+export const authService = new AuthService();
