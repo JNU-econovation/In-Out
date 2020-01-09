232204188561b03bd07938bceea0f9656e5834aa
Asua Hostin <a01054394111@gmail.com>
2020-01-09 16:45:06 +0900

    feat: 로그인을 한 상태이면 로그인 페이지 외의 화면에 접근을 하지 못하게 구현함.
    정확한 인원을 확인하기 위해서

--- client/src/presentation/App.jsx
+++ client/src/presentation/App.jsx
@@ -1,45 +1,57 @@
-import React, { Fragment } from "react";
+import React, { useEffect } from "react";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { MyPage } from "./pages/mypage";
 import { theme } from "./theme/theme";
 import Login from "./pages/home/login";
 import Register from "presentation/pages/home/register";
+import { PrivateRoute } from "presentation/components/private-route";
+import { useAuthDispatch, useAuthState } from "data/context/auth-context";
+import { Service } from "@service";
 
 function App() {
-  const Global = createGlobalStyle`
-  html{
-    box-sizing: border-box;
-  }
-    body{
-      margin: 0px;
-    }
+  const authDispatch = useAuthDispatch();
+  const auth = useAuthState();
+  useEffect(() => {
+    authDispatch({
+      type: "CHANGE",
+      value: Service.authService.isLogined()
+    });
+    console.log(auth);
+  }, [authDispatch]);
 
-    #root{
-      width: 100vw;
-    }
-  `;
   return (
-    <Fragment>
-      <ThemeProvider theme={theme}>
-        <Global></Global>
-        <Router>
-          <Switch>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-            <Route path="/register">
-              <Register></Register>
-            </Route>
-            <Route path="/my">
-              <MyPage />
-            </Route>
-            <Route path="/admin"></Route>
-          </Switch>
-        </Router>
-      </ThemeProvider>
-    </Fragment>
+    <ThemeProvider theme={theme}>
+      <Global></Global>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <PrivateRoute path="/register" auth={auth}>
+            <Register></Register>
+          </PrivateRoute>
+          <PrivateRoute path="/my" auth={auth}>
+            <MyPage />
+          </PrivateRoute>
+          <PrivateRoute path="/admin" auth={auth}></PrivateRoute>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
+
+const Global = createGlobalStyle`
+html{
+  box-sizing: border-box;
+}
+  body{
+    margin: 0px;
+  }
+
+  #root{
+    width: 100vw;
+  }
+`;
 
 export default App;
