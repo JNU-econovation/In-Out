6eca269a428a0b62dccea92dd2671db0176a71fd
Asua Hostin <a01054394111@gmail.com>
2020-01-09 18:49:06 +0900

    refactort: 쿼리스트링능 이용하는방식에서 패스파라미터를 이용하는 방식으로 변경
    더 편하고 직관적으로 path를 통해 데이터를 넘겨주기 위하여

--- server/controller/api/enroll.js
+++ server/controller/api/enroll.js
@@ -19,15 +19,14 @@ const showEnrollmentsByDate = async (req, res) => {
 };
 
 const showEnrollment = async (req, res) => {
-  const memberId = req.query.memberId;
+  const userMemberId = req.params.memberId;
   const today = dateHandler.getFormatDate(new Date());
-
+  console.log(userMemberId);
   try {
     const enrollment = await enrollmentRepository.findTodayById(
-      memberId,
+      userMemberId,
       today
     );
-
     return res.status(200).json({
       result: enrollment
     });
@@ -44,6 +43,7 @@ const createEnrollment = async (req, res) => {
   //평일 08:00 ~ 17:00가 아니거나 공휴일인 경우 출입 신청 불가 기능
   const memberId = req.body.memberId;
   const reason = req.body.reason;
+  console.log(memberId);
   const now = new Date();
   let result;
 
