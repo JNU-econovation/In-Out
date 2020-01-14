d4ced132c94fe0ce89093d2424208a81a64142fd
Asua Hostin <a01054394111@gmail.com>
2020-01-14 15:20:02 +0900

    feat: 토큰을 가지고 헤더에 저장하는 기능 구현
    axios 요청을 할 때 토큰을 가지고 있으면 헤더에 토큰을 담아 요청을 보낸다.

--- client/src/data/http-request/axios-wrapper.js
+++ client/src/data/http-request/axios-wrapper.js
@@ -1,12 +1,19 @@
 import axios from "axios";
-
+import { BrowserStorage } from "data/browser-storage";
 class AxiosWrapper {
   constructor() {
     this.axios = axios;
+    this.storage = new BrowserStorage("token", StorageType.LOCAL, tokenMapper);
   }
 
   getAxios() {
-    return this.axios.create();
+    const storage = this.storage.get();
+    if (storage.token) return this.axios.create();
+    return this.axios.create({
+      headers: {
+        "x-access-token": storage.token
+      }
+    });
   }
 }
 export const axioswrapper = new AxiosWrapper();
