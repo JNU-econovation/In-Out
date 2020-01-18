2d7ea80685d619877a6a78252f7ba3e1d00d363d
Asua Hostin <a01054394111@gmail.com>
2020-01-18 21:25:42 +0900

    feat : user 토큰 검증 기능 추가

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../../controller/api/user");
+const authController = require("./../../controller/api/auth");
 
+router.use(authController.verifyToken);
 router.put("/", userController.updatePassword);
 
 module.exports = router;
