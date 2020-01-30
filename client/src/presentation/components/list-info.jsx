6b72be1034a2877df943681abccf661265787c0a
Asua Hostin <a01054394111@gmail.com>
2020-01-30 18:41:47 +0900

    refactor: 인풋 자동완성방지 코드 추가

--- client/src/presentation/components/list-info.jsx
+++ client/src/presentation/components/list-info.jsx
@@ -30,6 +30,7 @@ export const ListInfoInput = ({
         onChange={onChange}
         onBlur={onBlur}
         type={inputType}
+        autoComplete="off"
       ></BlockInput>
       {children}
     </LineWrapper>
